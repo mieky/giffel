a92de0a281874093de429db3bcd27242291ffa9b
Mike Arvela <mike.arvela@futurice.com>
2015-12-12 18:19:21 +0200

    Add some defaults, longer animation

--- giffla.js
+++ giffla.js
@@ -11,6 +11,7 @@
 */
 
 var INTERVAL_SECONDS = 8;
+var ANIMATION_DURATION = 3;
 
 function loadImages(urls, callback) {
     if (urls.length === 0) {
@@ -66,7 +67,7 @@ function showImage(url) {
     if (oldImage) {
         setTimeout(function() {
             oldImage.parentNode.removeChild(oldImage);
-        }, 2000);
+        }, ANIMATION_DURATION * 1000);
     }
     var newImage = document.createElement("div");
     newImage.innerHTML = '<div class="current-image fullscreen fade-in" ' +
@@ -74,9 +75,9 @@ function showImage(url) {
     document.body.appendChild(newImage.firstChild);
 }
 
-// Try testing with e.g. http://localhost:8080/?http://i.imgur.com/HON37HH.gif,https://i.imgur.com/RNFpXtr.gif,http://i.imgur.com/rtjTZVP.gif,http://i.imgur.com/0hixygL.gif,http://i.imgur.com/N65P9gL.gif,http://i.imgur.com/9Siry0f.gif,http://i.imgur.com/sMilr1I.gif,https://i.imgur.com/97pDN6x.gif,http://i.imgur.com/Bk8j2Ax.gif,http://ak-hdl.buzzfed.com/static/2015-11/30/17/enhanced/webdr10/anigif_enhanced-1678-1448922451-13.gif,http://45.media.tumblr.com/2d6667e7a3d2bae53fa7b619be00e5ee/tumblr_n9y0x3HKeN1rv33k2o6_500.gif,http://i.imgur.com/Wmn1A0k.gif,https://49.media.tumblr.com/4f34bbb2eb2cb5e716bb81f6997e514c/tumblr_nykhdrhYGY1tl8u0ko1_400.gif
-
-var urls = window.location.search.substring(1).split(",");
+var defaults = "?http://i.imgur.com/sMilr1I.gif,http://i.imgur.com/Bk8j2Ax.gif,http://45.media.tumblr.com/2d6667e7a3d2bae53fa7b619be00e5ee/tumblr_n9y0x3HKeN1rv33k2o6_500.gif,http://i.imgur.com/HON37HH.gif,https://i.imgur.com/RNFpXtr.gif";
+var search = window.location.search || defaults;
+var urls = search.substring(1).split(",");
 
 var addImageCallback = cycleImages();
 loadImages(urls, addImageCallback);
